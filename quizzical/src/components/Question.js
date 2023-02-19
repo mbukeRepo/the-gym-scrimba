c9c312829f95f8b44f48f47f796d7c2aef2c501b
mbukeRepo <mbuke404@gmail.com>
2023-02-19 03:13:51 +0200

    feat: add readme for quizzical

---
+++ quizzical/src/components/Question.js
@@ -0,0 +1,70 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+import React from "react";
+import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
+import he from "he";
+
+const Question = ({ allAnswers, qID, question, isShowAnswers, updateHeld }) => {
+  const answerButtonsJSX = allAnswers.map((answer, index) => {
+    //Held Button Styles
+    let styles = {
+      backgroundColor: answer.isHeld
+        ? "var(--isHeld-bg-color)"
+        : "revert-layer",
+      color: answer.isHeld ? "var(--focused-btn-color)" : "revert-layer",
+    };
+
+    if (isShowAnswers) {
+      if (answer.isHeld && answer.isCorrect) {
+        styles = {
+          backgroundColor: "#94D7A2",
+          color: "var(--focused-btn-color)",
+        };
+      } else if (answer.isHeld && !answer.isCorrect) {
+        styles = {
+          backgroundColor: "#F8BCBC",
+          opacity: "50%",
+          border: "none",
+          color: "var(--focused-btn-color)",
+        };
+      } else if (answer.isCorrect) {
+        styles = {
+          backgroundColor: "#94D7A2",
+          color: "var(--focused-btn-color)",
+        };
+      } else if (!answer.isCorrect) {
+        styles = { opacity: "50%" };
+      }
+    }
+
+    return (
+      <button
+        key={nanoid()}
+        onClick={() => updateHeld(qID, answer.id)}
+        className="btn question__btn"
+        style={styles}
+        data-testid={`button${index}`}
+      >
+        {he.decode(allAnswers[index].value)}
+      </button>
+    );
+  }); // end of answerButtonsJSX
+
+  return (
+    <div className="question__component">
+      <h3 className="question__question">{he.decode(question)}</h3>
+      <div className="question__btnctr">{answerButtonsJSX}</div>
+    </div>
+  );
+};
+
+Question.propTypes = {
+  allAnswers: PropTypes.array,
+  qID: PropTypes.string,
+  question: PropTypes.string,
+  isShowAnswers: PropTypes.bool,
+  updateHeld: PropTypes.func,
+};
+
+export default React.memo(Question);
