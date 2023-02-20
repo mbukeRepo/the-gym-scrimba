2fe6836c04c7dcc367b9d918120258f443fa8c2c
mbukeRepo <mbuke404@gmail.com>
2023-02-20 11:18:20 +0200

    fix: some cleanup

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
     <div className="App">
       <Header />
       <Meme />
     </div>
   );
-}
+};
 export default App;
